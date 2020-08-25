67055e464909c69107ffe3cb698ce09dcbf19914
n-pretorious <pretoriousndungu@gmail.com>
2020-08-25 11:42:09 +0300

    Edit compenent to display content unhighlighted when highest vote for every vote is 0

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,9 +11,9 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'Act as if what you do makes a difference. It does', 'William James', 0, 0, new Date(2019, 6, 9), 'Mbappe'),
     // tslint:disable-next-line: max-line-length
-    new Quote(2, 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'Winston Churchill', 3, 1, new Date(2020, 11, 28), 'Messi'),
+    new Quote(2, 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'Winston Churchill', 0, 1, new Date(2020, 11, 28), 'Messi'),
     // tslint:disable-next-line: max-line-length
-    new Quote(3, 'Never bend your head. Always hold it high. Look the world straight in the eye', 'Helen Keller', 4, 5, new Date(2020, 7, 22), 'Martineli')
+    new Quote(3, 'Never bend your head. Always hold it high. Look the world straight in the eye', 'Helen Keller', 0, 5, new Date(2020, 7, 22), 'Martineli')
   ];
 
   constructor() { }
